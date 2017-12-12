6cc1b14811880c019eb79b0d2852e6b8f78e5562
Ravi Shrivastava <rsrivastava@ctepl.com>
2017-12-12 16:37:25 +0530

    done  with factory chapter

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,38 +3,45 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
 
 	before do
-		@user = User.create(
-			first_name: "Joe",
-			last_name: "Tester",
-			email: "joetester@example.com",
-			password: "dottle-nouveau-pavilion-tights-furze",
-		)
+		@user = FactoryGirl.create(:user)
+		project = FactoryGirl.create(:project, name: "Test Project", owner: @user)
+	end
 
-		@user.projects.create(
-			name: "Test Project",
-		)
+	it "can have many notes" do
+		project = FactoryGirl.create(:project, :with_notes)
+		expect(project.notes.length).to eq 5
 	end
 
   it "does not allow duplicate project names per user" do
-		new_project = @user.projects.build(
-			name: "Test Project",
-		)
+		new_project = FactoryGirl.build(:project, name: "Test Project", owner: @user)
 		new_project.valid?
 		expect(new_project.errors[:name]).to include("has already been taken")
 	end
 
 	it "allows two users to share a project name" do
-		other_user = User.create(
-			first_name: "Ravi",
-			last_name: "Shrivastava",
-			email: "rsravishri30@gmail.com",
-			password: "Tester123",
-		)
+		other_user = FactoryGirl.create(:user)
 
 		other_project = other_user.projects.build(
-			name: "Test Project",
+			name: "Test Project",owner: other_user
 		)
 
 		expect(other_project).to be_valid
+	end
+
+	describe "late status" do
+		it "is late when the due date is past today" do
+			project = FactoryGirl.create(:project, :due_yesterday)
+			expect(project).to be_late
+		end
+
+		it "is on time when the due date is today" do
+			project = FactoryGirl.create(:project, :due_today)
+			expect(project).to_not be_late
+		end
+
+		it "is on time when the due date is in the future" do
+			project = FactoryGirl.create(:project, :due_tomorrow)
+			expect(project).to_not be_late
+		end
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,58 +2,42 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
   it "is valid with a first name, last name, email, and password" do
-  	user = User.new(
-  			first_name: 'Ravi',
-  			last_name: 'Shrivastava',
-  			email: 'rsravishri30@gmail.com',
-  			password: "Tester123"
-  		)
-  	expect(user).to be_valid
+    user = FactoryGirl.build(:user)
+    expect(user).to be_valid
   end
 
-	it "is invalid without a first name" do
-  	user = User.new(first_name: nil)
-  	user.valid?
-  	expect(user.errors[:first_name]).to include("can't be blank")
+  it "is invalid without a first name" do
+    user = FactoryGirl.build(:user, first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
   end
 
-	it "is invalid without a last name" do
-  	user = User.new(last_name: nil)
-  	user.valid?
-  	expect(user.errors[:last_name]).to include("can't be blank")
+  it "is invalid without a last name" do
+    user = FactoryGirl.build(:user, last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-	it "is invalid without an email address" do
-		user = User.new(email: nil)
-  	user.valid?
-  	expect(user.errors[:email]).to include("can't be blank")
+  it "is invalid without an email address" do
+    user = FactoryGirl.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
-	it "is invalid with a duplicate email address" do
-		User.create(
-  			first_name: 'Ravi',
-  			last_name: 'Shrivastava',
-  			email: 'rsravishri30@gmail.com',
-  			password: "Tester123"
-  	)
-		user = User.new(
-  			first_name: 'Ravi',
-  			last_name: 'Shrivastava',
-  			email: 'rsravishri30@gmail.com',
-  			password: "Tester123"
-  	)
-  	user.valid?
-  	expect(user.errors[:email]).to include("has already been taken")
+  it "is invalid with a duplicate email address" do
+    FactoryGirl.create(:user, email: 'rsravishri30@gmail.com')
+    user = FactoryGirl.build(:user, email: 'rsravishri30@gmail.com')
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
   end
 
-	it "returns a user's full name as a string" do
-  		user = User.new(
-  			first_name: 'Ravi',
-  			last_name: 'Shrivastava',
-  			email: 'rsravishri30@gmail.com',
-  			password: "Tester123"
-  		)
-  		expect(user.name).to eq("Ravi Shrivastava")
+  it "returns a user's full name as a string" do
+    user = FactoryGirl.build(:user,first_name: 'Ravi',last_name: 'Shrivastava')
+ 		expect(user.name).to eq("Ravi Shrivastava")
 	end
 end
